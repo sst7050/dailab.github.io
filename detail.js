b6c6cbf3045649307fb6e3f42dae684bf448527b
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2024-07-17 11:19:26 +0900

    nav바 100vw로 수정, 펼칠 때 검은 오버레이 적용

--- detail.js
+++ detail.js
@@ -1,13 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuTrigger = document.querySelector('.menu-trigger');
     const sideMenu = document.querySelector('.side_menu');
+    const overlay = document.querySelector('.overlay');
 
-    menuTrigger.addEventListener('click', function() {
+    menuTrigger.addEventListener('click', function(event) {
+        event.preventDefault();
         menuTrigger.classList.toggle('active-1');
-        if (sideMenu.style.display === "block") {
-            sideMenu.style.display = "none";
+        if (sideMenu.classList.contains('active')) {
+            sideMenu.classList.remove('active');
+            sideMenu.style.height = '0';
+            overlay.classList.remove('active');
         } else {
-            sideMenu.style.display = "block";
+            sideMenu.classList.add('active');
+            sideMenu.style.height = sideMenu.scrollHeight + 'px';
+            overlay.classList.add('active');
         }
+    });
+
+    overlay.addEventListener('click', function() {
+        sideMenu.classList.remove('active');
+        sideMenu.style.height = '0';
+        overlay.classList.remove('active');
+        menuTrigger.classList.remove('active-1');
     });
 });
